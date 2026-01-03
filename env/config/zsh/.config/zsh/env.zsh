9019789aa2389d68dc09e72ad271786bf4b3ee1b
Aviral Dubey <aviralkmdubey@gmail.com>
2026-01-03 22:08:19 +0530

    Commited on Jan 3rd 2026 : 10:08 PM

--- env/config/zsh/.config/zsh/env.zsh
+++ env/config/zsh/.config/zsh/env.zsh
@@ -4,8 +4,11 @@
 # --------------------------------------
 
 export PATH="$HOME/.local/bin:$HOME/.config/tmux/plugins/tmuxifier/bin:$PATH"
+
 export EDITOR="nvim"
 export GTK_IM_MODULE=simple
+
+export XDG_CONFIG_HOME="$HOME/.config"
 
 # My secrets directory
 export SECRETS_DIR="$HOME/.secrets/"
