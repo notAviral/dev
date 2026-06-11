5d81076b5c3d8627434604571dfb7d5043ded0e5
Aviral Dubey <aviralkmdubey@gmail.com>
2026-06-11 18:46:07 +0530

    fix session manager for tmux

--- config/zsh/.config/zsh/git.zsh
+++ config/zsh/.config/zsh/git.zsh
@@ -7,8 +7,9 @@ alias ga='git add'
 alias gap='git add --patch'
 alias gaa='git add .'
 alias gc='git commit'
-# alias gp='git push'
-# alias gu='git pull'
+alias gca='git commit -a'
+alias gp='git push'
+alias gu='git pull'
 alias gcl='git clone'
 alias gl='git log --graph --all --decorate --date=iso --pretty=format:"%C(auto)%h%Creset %C(bold blue)(%ad)%Creset %C(yellow)%d%Creset%n%C(bold green)Author:%Creset %an <%ae>%n%C(bold red)Committer:%Creset %cn <%ce>%n%C(bold cyan)Message:%Creset %s%n"'
 
@@ -31,12 +32,3 @@ zle -N lazygit_widget
 bindkey -M emacs '^g' lazygit_widget
 bindkey -M vicmd '^g' lazygit_widget
 bindkey -M viins '^g' lazygit_widget
-
-
-zle -N lazygit_widget
-
-# Bind Ctrl+G (ASCII 0x07)
-bindkey -M emacs '^g' lazygit_widget
-bindkey -M vicmd '^g' lazygit_widget
-bindkey -M viins '^g' lazygit_widget
-
