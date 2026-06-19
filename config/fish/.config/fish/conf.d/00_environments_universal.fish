ece48d4878bb4efd3decff0db5da924564786554
Aviral Dubey <aviralkmdubey@gmail.com>
2026-06-19 18:37:19 +0530

    fix(fish): add $SHELL variable

--- config/fish/.config/fish/conf.d/00_environments_universal.fish
+++ config/fish/.config/fish/conf.d/00_environments_universal.fish
@@ -4,6 +4,7 @@ set -Ux XDG_HOME_CACHE  ~/.cache
 set -Ux SECRETS_DIR $HOME/.secrets/
 set -Ux NOTES_DIR   $HOME/Notes/
 
+set -Ux SHELL /bin/fish
 set -Ux EDITOR nvim
 
 set -Ux GTK_IM_MODULE simple
