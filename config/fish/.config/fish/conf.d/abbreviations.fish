0e143b54415f69c386760bed9d7bf218f13a296a
Aviral Dubey <aviralkmdubey@gmail.com>
2026-06-23 19:55:32 +0530

    feat(fish): add better command level abbriviations

--- config/fish/.config/fish/conf.d/abbreviations.fish
+++ config/fish/.config/fish/conf.d/abbreviations.fish
@@ -14,15 +14,6 @@ if status is-interactive
 
     abbr -a lg "lazygit"
 
-    abbr -a ga "git add"
-    abbr -a gs "git status"
-    abbr -a gc "git commit"
-    abbr -a gd "git diff"
-    abbr -a gp "git push"
-    abbr -a gu "git pull"
-    abbr -a gaa "git add ."
-    abbr -a gap "git add --patch"
-    abbr -a gcl "git clone"
-    abbr -a gca "git commit -a"
+    abbr -a g "git"
 
 end
---
+++ config/fish/.config/fish/conf.d/app_abbrs.fish
@@ -0,0 +1,19 @@
+
+if status is-interactive 
+
+    # Git Abbriviations 
+    abbr -a -c git a  "add"
+    abbr -a -c git d  "diff"
+    abbr -a -c git p  "push"
+    abbr -a -c git u  "pull"
+    abbr -a -c git cm "commit -m"
+    abbr -a -c git ca "commit -a"
+    abbr -a -c git aa "add ."
+    abbr -a -c git ap "add --patch"
+    abbr -a -c git cl "clone"
+
+    # Cargo Abbriviations
+    abbr -a -c cargo r  "run"
+    abbr -a -c cargo b  "build"
+    abbr -a -c cargo br "build --release"
+end
